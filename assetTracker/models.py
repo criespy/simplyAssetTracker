49f082a68bb354cb0c6107f03e6a0aecf99cf697
Aries Wibowo <riespratama@gmail.com>
2023-05-19 17:03:09 +0700

    Test models for simplyAuth

--- assetTracker/models.py
+++ assetTracker/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Kategori(models.Model):
+    nama_kategori = models.CharField(max_length=32)
+
+class Lokasi(models.Model):
+    nama_lokasi = models.CharField(max_length=32)
+
+class StatusAsset(models.Model):
+    status = models.CharField(max_length=16)
+
+class Manufaktur(models.Model):
+    nama_manufaktur = models.CharField(max_length=32)
+
+class Asset(models.Model):
+    nama_asset = models.CharField(max_length=32)
+    model = models.CharField(max_length=32)
+    serialnumber = models.CharField(max_length=32)
+    kategori = models.ForeignKey(Kategori, on_delete=models.CASCADE)
+    manufaktur = models.ForeignKey(Manufaktur, on_delete=models.CASCADE)
+    tanggalpembelian = models.DateField()
+    lamagaransi = models.IntegerField(max_length=3) #dalam bulan
+    catatan = models.CharField(max_length=256)
+    status = models.ForeignKey(StatusAsset, on_delete=models.CASCADE)
+    lokasi = models.ForeignKey(Lokasi, on_delete=models.CASCADE)
+
+
+
--- simplyAuthenticator/views.py
+++ simplyAuthenticator/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.models import User
+from django.views import View
 
 class simplyLogin(LoginView):
     template_name = 'login.html'
@@ -10,3 +12,32 @@ class simplyLogout(LogoutView):
         logout(request)
     template_name = 'login.html'
     next_page = 'scanner'
+
+class ManageUserView(View):
+    def get(self, request):
+        pengguna = User.objects.all()
+        return render(request, 'manage_pengguna.html', {'users': pengguna})
+    
+class CreateUserView(View):
+    def post(self, request):
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        User.objects.create_user(username=username, email=email, password=password)
+        return redirect('manage_users')
+    
+class EditUserView(View):
+    def get(self, request, user_id):
+        user = User.objects.get(id=user_id)
+        return render(request, 'edit_user.html', {'user': user})
+
+    def post(self, request, user_id):
+        user = User.objects.get(id=user_id)
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        user.username = username
+        user.email = email
+        user.set_password(password)
+        user.save()
+        return redirect('manage_users')
